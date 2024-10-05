68538c6d30e192dbf94f65428dac7690c579add3
Nicholas <2201124>
2024-10-05 14:38:23 +0800

    Added the bias correction for gyro readings

--- Project_2_MPU9250/Project_2_MPU9250.ino
+++ Project_2_MPU9250/Project_2_MPU9250.ino
@@ -1,5 +1,4 @@
 #include "FastIMU.h"
-#include <Wire.h>
 
 #define IMU_ADDRESS 0x68
 
@@ -9,9 +8,15 @@ calData calib = { 0 };  // Calibration data
 GyroData gyroData;
 AccelData accelData;
 
+//offsets
 float AccelXOffset = 0.03;
 float AccelYOffset = 0.02;
 float AccelZOffset = 1.09;
+float gyroXOffset = 0, gyroYOffset = 0, gyroZOffset = 0;
+
+static float accelXSum = 0, accelYSum = 0, accelZSum = 0;
+static float gyroXSum = 0, gyroYSum = 0, gyroZSum = 0;
+bool calibrated = false;
 
 void setup() {
   Serial.begin(115200);
@@ -21,55 +26,91 @@ void setup() {
 
   int err = mpu.init(calib, IMU_ADDRESS);
   if (err != 0) {
-    Serial.print("Error initializing IMU: ");
+    Serial.print("Error initializing IMU: "); 
     Serial.println(err);
     while (true) {
       ;
     }
   } else {
     Serial.println("MPU6500 Initialized");
+    calibrateGyro();
   }
 }
 
 void loop() {
-  float accelXSum = 0, accelYSum = 0, accelZSum = 0;
+  if (!calibrated) return;
+
+  static int count = 0;
+  mpu.update();
+  mpu.getAccel(&accelData);
+  mpu.getGyro(&gyroData);
+
+  // Apply gyro offsets
+  float gyroX = gyroData.gyroX - gyroXOffset;
+  float gyroY = gyroData.gyroY - gyroYOffset;
+  float gyroZ = gyroData.gyroZ - gyroZOffset;
+
+  accelXSum += (accelData.accelX - AccelXOffset);
+  accelYSum += (accelData.accelY - AccelYOffset);
+  accelZSum += (accelData.accelZ - AccelZOffset);
+
+  gyroXSum += gyroX;
+  gyroYSum += gyroY;
+  gyroZSum += gyroZ;
+  count++;
+
+  if (count == 10) {
+    float avgAccelX = accelXSum / 10.0;
+    float avgAccelY = accelYSum / 10.0;
+    float avgAccelZ = accelZSum / 10.0;
+
+    float avgGyroX = gyroXSum / 10.0;
+    float avgGyroY = gyroYSum / 10.0;
+    float avgGyroZ = gyroZSum / 10.0;
+
+    Serial.print("Average Accel : "); Serial.print(avgAccelX); Serial.print(", ");
+    Serial.print(avgAccelY); Serial.print(", ");
+    Serial.println(avgAccelZ);
+
+    Serial.print("Average Gyro  : "); Serial.print(avgGyroX); Serial.print(", ");
+    Serial.print(avgGyroY); Serial.print(", ");
+    Serial.println(avgGyroZ);
+
+    accelXSum = 0;
+    accelYSum = 0;
+    accelZSum = 0;
+    gyroXSum = 0;
+    gyroYSum = 0;
+    gyroZSum = 0;
+    count = 0;
+  }
+  delay(100);
+}
+
+
+
+void calibrateGyro() {
+  Serial.println("Calibrating Gyroscope");
   float gyroXSum = 0, gyroYSum = 0, gyroZSum = 0;
+  const int numSamples = 300;
 
-  // Collect 10 readings and sum them up
-  for (int i = 0; i < 10; i++) {
-    // Get sensor data
+  for (int i = 0; i < numSamples; i++) {
     mpu.update();
-    mpu.getAccel(&accelData);
     mpu.getGyro(&gyroData);
-
-    // Accumulate Accelerometer readings
-    accelXSum += (accelData.accelX - AccelXOffset);
-    accelYSum += (accelData.accelY - AccelYOffset);
-    accelZSum += (accelData.accelZ - AccelZOffset);
-
-    // Accumulate Gyroscope readings
     gyroXSum += gyroData.gyroX;
     gyroYSum += gyroData.gyroY;
     gyroZSum += gyroData.gyroZ;
 
-    delay(100); // Delay between readings to control sampling rate
+    delay(10); // Delay between readings to ensure stable averaging
   }
 
-  // Calculate the average after 10 readings
-  float avgAccelX = accelXSum / 10.0;
-  float avgAccelY = accelYSum / 10.0;
-  float avgAccelZ = accelZSum / 10.0;
+  gyroXOffset = gyroXSum / numSamples;
+  gyroYOffset = gyroYSum / numSamples;
+  gyroZOffset = gyroZSum / numSamples;
 
-  float avgGyroX = gyroXSum / 10.0;
-  float avgGyroY = gyroYSum / 10.0;
-  float avgGyroZ = gyroZSum / 10.0;
-
-  // Print averaged values
-  Serial.print("Average Accel : "); Serial.print(avgAccelX); Serial.print(", ");
-  Serial.print(avgAccelY); Serial.print(", ");
-  Serial.println(avgAccelZ);
-
-  Serial.print("Average Gyro  : "); Serial.print(avgGyroX); Serial.print(", ");
-  Serial.print(avgGyroY); Serial.print(", ");
-  Serial.println(avgGyroZ);
+  Serial.print("Gyro Calibration Complete. Offsets Values are: ");
+  Serial.print("X: "); Serial.print(gyroXOffset); Serial.print(", ");
+  Serial.print("Y: "); Serial.print(gyroYOffset); Serial.print(", ");
+  Serial.print("Z: "); Serial.println(gyroZOffset);
+  calibrated = true;
 }
