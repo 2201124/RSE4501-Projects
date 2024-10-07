33c2a64abd7a093cf93fd0af28669a1bb4b0e0a2
Nicholas <2201124>
2024-10-07 14:20:52 +0800

    Added the Whatsapp Messaging Capability using CallMeBot API

--- Project_2_MPU9250/Lab4_Arduino-ESP-Esp-Client/Lab4_Arduino-ESP-Esp-Client.ino
+++ Project_2_MPU9250/Lab4_Arduino-ESP-Esp-Client/Lab4_Arduino-ESP-Esp-Client.ino
@@ -1,3 +1,10 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <UrlEncode.h>
+
+
+
 const int incomingPin = D5; 
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
@@ -8,26 +15,65 @@ int signalState = LOW;
 void setup() {
   Serial.begin(115200);
   pinMode(incomingPin, INPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  sendMessage("Hello from ESP32!");
 }
 
 void loop() {
-  int reading = digitalRead(incomingPin);
+  // int reading = digitalRead(incomingPin);
 
-  if (reading != lastSignalState) {
-    lastDebounceTime = millis();  // Reset the debounce timer
+  // if (reading != lastSignalState) {
+  //   lastDebounceTime = millis();  // Reset the debounce timer
+  // }
+
+  // if ((millis() - lastDebounceTime) > debounceDelay) {
+  //   if (reading != signalState) {
+  //     signalState = reading;
+
+  //     if (signalState == HIGH) {
+  //       Serial.println("Received HIGH signal from Arduino!");
+  //     } else {
+  //       Serial.println("Received LOW signal from Arduino.");
+  //     }
+  //   }
+  // }
+
+  // lastSignalState = reading;
+}
+
+
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST
+  String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+  WiFiClient client;    
+  HTTPClient http;
+  http.begin(client, url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
   }
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != signalState) {
-      signalState = reading;
-
-      if (signalState == HIGH) {
-        Serial.println("Received HIGH signal from Arduino!");
-      } else {
-        Serial.println("Received LOW signal from Arduino.");
-      }
-    }
-  }
-
-  lastSignalState = reading;
+  // Free resources
+  http.end();
 }
